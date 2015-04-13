e98ca85d6968e852935a3bdc41c18841b0115d38
leaveys <leaveys@users.noreply.github.com>
2015-04-13 13:48:41 -0500

    Update workflow.groovy

--- workflow.groovy
+++ workflow.groovy
@@ -1,3 +1,25 @@
+stage: 'commit'
+node('master') {
+  git url: 'https://github.com/jenkinsci/parallel-test-executor-plugin-sample.git'
+  archive 'pom.xml, src/'
+}
+def splits = splitTests([$class: 'CountDrivenParallelism', size: 2])
+def branches = [:]
+for (int i = 0; i < splits.size(); i++) {
+  def exclusions = splits.get(i);
+  branches["split${i}"] = {
+    node('master') {
+      sh 'rm -rf *'
+      unarchive mapping: ['pom.xml' : '.', 'src/' : '.']
+      writeFile file: 'exclusions.txt', text: exclusions.join("\n")
+      sh "${tool 'M3'}/bin/mvn -B -Dmaven.test.failure.ignore test"
+      step([$class: 'JUnitResultArchiver', testResults: 'target/surefire-reports/*.xml'])
+    }
+  }
+}
+parallel branches
+
+stage 'Acceptance'
 node('master') {
   git url: 'https://github.com/jenkinsci/parallel-test-executor-plugin-sample.git'
   archive 'pom.xml, src/'
